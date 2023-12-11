692c5d9a400a32664733d5aa3584c876efd11b7b
kim-elijah-sol <kimelijahsol@gmail.com>
2023-12-11 13:58:43 +0900

    chore: [apps/next-a] dist 폴더 제거

--- packages/react/components/dist/packages/react/components/src/Button.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-export declare function Button({ type, style, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>): React.JSX.Element;
-//# sourceMappingURL=Button.d.ts.map
