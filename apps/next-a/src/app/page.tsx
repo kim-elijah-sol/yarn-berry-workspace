45b4af8116a08818552b601a5e99b11bfd0f6276
kim-elijah-sol <kimelijahsol@gmail.com>
2023-12-11 17:30:51 +0900

    env: [apps/next-a] 공통 hooks 사용

--- apps/next-a/src/app/page.tsx
+++ apps/next-a/src/app/page.tsx
@@ -2,16 +2,21 @@
 
 import { useEffect } from 'react'
 import { Button } from '@ybw/react-components'
+import { useCount } from '@ybw/react-hooks'
 import { sum } from '@ybw/common-utils'
 
 export default function Home() {
+  const { count, increment, decrement } = useCount()
+
   useEffect(() => {
     console.log(sum(1, 2, 3))
   }, [])
 
   return (
     <main>
-      <Button>Click Me</Button>
+      <p>{count}</p>
+      <Button onClick={increment}>+ 1</Button>
+      <Button onClick={decrement}>- 1</Button>
     </main>
   )
 }
