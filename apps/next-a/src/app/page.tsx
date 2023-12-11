bf6d8d5d1a9821785aaac8d69e9af737703bd575
kim-elijah-sol <kimelijahsol@gmail.com>
2023-12-11 12:12:56 +0900

    feat: [apps/next-a] 공통 유틸리티 함수 사용

--- apps/next-a/src/app/page.tsx
+++ apps/next-a/src/app/page.tsx
@@ -1,6 +1,14 @@
+'use client'
+
+import { useEffect } from 'react'
 import { Button } from '@ybw/react-components'
+import { sum } from '@ybw/common-utils'
 
 export default function Home() {
+  useEffect(() => {
+    console.log(sum(1, 2, 3))
+  }, [])
+
   return (
     <main>
       <Button>Click Me</Button>
