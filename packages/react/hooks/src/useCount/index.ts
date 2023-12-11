3850d24c62115d3411a0b3d8a62e878de6ebce77
kim-elijah-sol <kimelijahsol@gmail.com>
2023-12-11 17:30:15 +0900

    env: react hooks package 추가

---
+++ packages/react/hooks/src/useCount/index.ts
@@ -0,0 +1,15 @@
+import { useCallback, useState } from 'react'
+
+export function useCount() {
+  const [count, setCount] = useState(0)
+
+  const increment = useCallback(() => {
+    setCount((x) => x + 1)
+  }, [])
+
+  const decrement = useCallback(() => {
+    setCount((x) => x - 1)
+  }, [])
+
+  return { count, increment, decrement }
+}
